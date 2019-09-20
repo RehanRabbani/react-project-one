69b0c5b5ef3c0af5a7ab8635d8d2bf7c0a302ce7
RehanRabbani <rabbanirehan6@gmail.com>
2019-09-20 12:09:47 +0500

    initial

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+class Boo extends React.Component{
+    render(){
+        return(
+            <Button variant="primary" size="lg" active>
+            Primary button
+            </Button>
+        )
+    }
+}
+export default Boo;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './App.css';
+import axios from 'axios';
+import Card from 'react-bootstrap/Card';
+import Header from './components/Header';
+import Boo from './components/Button';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Home extends React.Component{
+constructor(props){
+    super(props);
+    this.state={
+        photos:[]
+    }
+}
+componentDidMount(){
+    axios.get(`https://jsonplaceholder.typicode.com/photos`)
+    .then(json=>{
+        console.log(json.data);
+        const photos = json.data;
+        console.log(photos);
+        this.setState({photos:photos})
+    })
+}
+
+    render(){
+        let {photos}=this.state;
+        return(
+            <section>
+            <Header/>
+         
+            <div className="parent">
+            
+              
+               {
+                this.state.photos.map(item=>{return(<Card style={{ width: '18rem' }}
+                onClick={()=>{alert("Click")}} className="card-sty">
+                <Card.Img variant="top" src={item.url} className="img-size" />
+                <p>{item.title}</p>
+                </Card>)}) 
+                
+
+               }
+            </div>
+            </section>
+        )
+    }
+}
+
+ReactDOM.render(<Home />, document.getElementById('root'));
+
